53d6e5398de1674e32d20699a42f74e186d999df
Brendon <brendonqueiros@hotmail.com>
2022-12-07 01:11:08 -0300

    Adicionando instancia do client ao abrir a aplicação

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
  
 from src.views.home import Home
 from src.views.board import Board
+from src.client import Client
 class Main:
     def setup(self):
 
@@ -9,17 +10,19 @@ class Main:
             self.clock.tick(30)
             if(self.tela == "home"):
                 self.home.run()
+                
             elif(self.tela=="entrar"):
                 self.board.run()
     
     def __init__(self):
-        pygame.init()
+        pygame.init()   
         self.screen = pygame.display.set_mode((1280, 720))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Stop XOU")
         self.tela = "home"
+        self.client = Client()
         self.home = Home(self,self.screen)
-        self.board = Board(self,self.screen)
+        self.board = Board(self,self.screen,self.client) 
         self.setup()
         
 
